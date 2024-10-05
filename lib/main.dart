a0ec1aa2e536634047db2620fe400753f1045f2e
rangguy <ranggadwi100@gmail.com>
2024-10-05 20:46:26 +0700

    add format and route

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,40 @@
+import 'package:course_hotelio/config/app_route.dart';
 import 'package:course_hotelio/firebase_options.dart';
+import 'package:course_hotelio/page/home_page.dart';
+import 'package:course_hotelio/page/intro_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // menginisialisasi date format yang akan digunakan pada aplikasi
+  initializeDateFormatting('en_US');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) {
+          return const IntroPage();
+        },
+        AppRoute.intro: (context) => const IntroPage(),
+        AppRoute.home: (context) => const HomePage(),
+        AppRoute.signin: (context) => const IntroPage(),
+        AppRoute.detail: (context) => const IntroPage(),
+        AppRoute.checkout: (context) => const IntroPage(),
+        AppRoute.checkoutSuccess: (context) => const IntroPage(),
+        AppRoute.detailBooking: (context) => const IntroPage(),
+      },
     );
   }
 }
