31f397a66da2069d67c3c3b03873a6120b6a8134
rangguy <ranggadwi100@gmail.com>
2024-10-12 00:40:31 +0700

    slicing nearby page part 1 done

--- lib/page/nearby_page.dart
+++ lib/page/nearby_page.dart
@@ -1,3 +1,5 @@
+import 'package:course_hotelio/config/app_asset.dart';
+import 'package:course_hotelio/config/app_color.dart';
 import 'package:flutter/material.dart';
 
 class NearbyPage extends StatelessWidget {
@@ -9,8 +11,110 @@ class NearbyPage extends StatelessWidget {
       children: [
         const SizedBox(
           height: 24,
-        )
+        ),
+        header(context),
+        const SizedBox(
+          height: 20,
+        ),
+        searchField(),
+        const SizedBox(
+          height: 30,
+        ),
       ],
+    );
+  }
+
+  Container searchField() {
+    return Container(
+      height: 45,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Stack(
+        children: [
+          Container(
+            height: 45,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'Search by name or city',
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14,
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Material(
+              color: AppColor.secondary,
+              borderRadius: BorderRadius.circular(45),
+              child: InkWell(
+                onTap: () {},
+                borderRadius: BorderRadius.circular(45),
+                child: const SizedBox(
+                  height: 45,
+                  width: 45,
+                  child: Center(
+                    child: ImageIcon(
+                      AssetImage(AppAsset.iconSearch),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding header(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: Image.asset(
+              AppAsset.profile,
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                'Near Me',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      fontWeight: FontWeight.w900,
+                    ),
+              ),
+              const Text(
+                '100 Hotels',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
