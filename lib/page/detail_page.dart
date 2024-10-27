801c66ffd812fae40f27655c491449bba29348ea
rangguy <ranggadwi100@gmail.com>
2024-10-27 20:34:01 +0700

    detail page part 2:2

--- lib/page/detail_page.dart
+++ lib/page/detail_page.dart
@@ -1,9 +1,29 @@
+import 'package:course_hotelio/config/app_asset.dart';
 import 'package:course_hotelio/config/app_color.dart';
 import 'package:course_hotelio/model/hotel.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
-  const DetailPage({super.key});
+  DetailPage({super.key});
+
+  final List facilities = [
+    {
+      "icon": AppAsset.iconCoffee,
+      "label": 'Lounge',
+    },
+    {
+      "icon": AppAsset.iconOffice,
+      "label": 'Office',
+    },
+    {
+      "icon": AppAsset.iconWifi,
+      "label": 'Wi-Fi',
+    },
+    {
+      "icon": AppAsset.iconStore,
+      "label": 'Store',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -55,9 +75,57 @@ class DetailPage extends StatelessWidget {
                     ),
               ),
             ),
+            gridFacilities(),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              child: Text(
+                'Activities',
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            SizedBox(
+              height: 105,
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  GridView gridFacilities() {
+    return GridView.builder(
+      padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+      itemCount: facilities.length,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+      ),
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey[200]!),
+            borderRadius: BorderRadius.circular(24),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ImageIcon(AssetImage(facilities[index]['icon'])),
+              const SizedBox(
+                height: 4,
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 
